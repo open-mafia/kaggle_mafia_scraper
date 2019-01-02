d41cda1cf805af54bb24d2dff10472bc59d9c589
Anatoly Makarevich <anatoly_mak@yahoo.com>
2019-01-03 02:00:24 +0300

    Added dataset splitter and exporting notebook.

--- src/bay12_scraper/prep/__init__.py
+++ src/bay12_scraper/prep/__init__.py
@@ -45,3 +45,74 @@ def load_or_create_posts(fname, roles=None, threads=None):
     except Exception:
         logging.exception('Could not write to CSV file. Check dir exists?')
     return posts
+
+
+def fix_roles_df(df_roles):
+    df_roles_fixed = df_roles.copy()
+
+    tbr = (df_roles_fixed['role'] == 'replaced')
+    df_roles_fixed['replacement_depth'] = 0
+
+    cols_fixed = list(df_roles.columns) + ['replacement_depth']
+
+    while tbr.sum():
+        df_roles_fixed = df_roles_fixed.merge(
+            df_roles_fixed, how='left', 
+            left_on=['thread_num', 'replaced_by'], 
+            right_on=['thread_num', 'user'], 
+            suffixes=('', '_repl')
+        )[cols_fixed + ['role_repl']]
+        
+        tbr = (df_roles_fixed['role'] == 'replaced')
+        df_roles_fixed['role'][tbr] = df_roles_fixed['role_repl'][tbr]
+        df_roles_fixed['replacement_depth'][tbr] += 1
+        df_roles_fixed = df_roles_fixed[cols_fixed]
+    
+    return df_roles_fixed
+
+
+def split_ds(roles, posts, pct_public=0.2, pct_private=0.2):
+    """Splits the dataset into 3 parts: train, test (public), test (private).
+    
+    Returns dict of [posts, roles] for keys ['train', 'public', 'private'].
+    """
+
+    pct_train = 1 - pct_public - pct_private
+    assert min(pct_train, pct_public, pct_private) > 0
+    
+    t_nums = sorted(roles['thread_num'].unique())
+    
+    # Very simple time-based split, since thread numbers are chronological 
+    # I know this introduces a bias - will probably change later
+    
+    n_prv = int(pct_private * len(t_nums))
+    n_pub = int(pct_public * len(t_nums))
+    
+    t_prv = t_nums[-n_prv:]
+    t_pub = t_nums[-(n_prv + n_pub): -n_prv]
+    # NOTE: The remainder will round up, which is OK 
+    
+    # Split the posts
+    pi_prv = posts.thread_num.isin(t_prv)
+    pi_pub = posts.thread_num.isin(t_pub)
+    
+    p_private = posts[pi_prv]
+    p_public = posts[pi_pub]
+    p_train = posts[~(pi_pub | pi_prv)]
+    
+    # Split the roles
+    ri_prv = roles.thread_num.isin(t_prv)
+    ri_pub = roles.thread_num.isin(t_pub)
+    
+    r_private = roles[ri_prv]
+    r_public = roles[ri_pub]
+    r_train = roles[~(ri_pub | ri_prv)]
+    
+    # Return them D:
+    
+    res = {
+        'train': [p_train, r_train], 
+        'public': [p_public, r_public], 
+        'private': [p_private, r_private]
+    }
+    return res
